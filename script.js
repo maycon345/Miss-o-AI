baa4f3e1090b29af843251e7fb8266ab12f81ca7
maycon345 <146994229+maycon345@users.noreply.github.com>
2025-08-04 11:51:53 -0300

    .

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+
+const caixaAlteenativas = document.querySelector('.caixa-alternativa');
+
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const caixaResultado = document.querySelector('.caixa-Resultado');
+
+const lista = [];
+
+const perguntas [
+    {
+        enunciado: "Pergunta 1",
+        alteenativas: [
+"Alternativas 1"
+"Alteenativas 2"
+
+    ]
+} //fecha objeto pergunta
+] // fecha lista
